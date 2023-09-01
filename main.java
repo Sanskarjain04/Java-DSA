4eded6d509317965a916e8e90f5dbad680572e63
Sanskarjain04 <116282600+Sanskarjain04@users.noreply.github.com>
2023-09-01 22:26:49 +0530

    Created main.java and stars.java
    In main.java I learned how to print a statement and different ways to print them in different lines.
    In stars.java I print a star pattern.

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args){
         //output
-        System.out.print("Hello World with Java");
+        System.out.println("Hello World with Java");
+        System.out.println("Hello World with Java");
+        System.out.print("Hello World with Java\nHow are you");
     }
 }
---
+++ stars.java
@@ -0,0 +1,8 @@
+public class stars {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
